bb37e409c2009d4c67d9a0fa35ca49ccac1b1a45
Alex Wong <alexwong024@gmail.com>
2018-04-11 20:29:29 -0400

    Drag and Drop

---
+++ Assets/Systems/Peeples/DraggablePeeple.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggablePeeple : MonoBehaviour, IDragHandler, IDropHandler {
+    [SerializeField] PeepleFigurine peepleFigurine;
+
+    public void OnDrag(PointerEventData eventData)	{
+        transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+        
+	}
+
+    public void OnDrop(PointerEventData eventData) {
+        if (RectTransformUtility.RectangleContainsScreenPoint(DragManager.DropZone, eventData.position)) {
+            Debug.Log("Dropped in Zone");
+        } else {
+            Debug.Log("Dropped nowhere");
+        }
+        transform.localPosition = new Vector3(0, 0, 0);
+    }
+}
---
+++ Assets/Systems/UI/DragManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragManager : MonoBehaviour {
+    [SerializeField] RectTransform dropZone; //For playing Peeples
+
+    private static DragManager instance;
+
+    private PeepleFigurine draggingPeeple = null;
+
+    public static PeepleFigurine DraggingPeeple { get { return instance.draggingPeeple; } }
+
+    public static RectTransform DropZone { get { return instance.dropZone; } }
+
+	private void Awake() {
+        instance = this;
+	}
+}
